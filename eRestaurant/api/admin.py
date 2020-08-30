0af89f23c7b2a3031a070fa8efa507b9a6243a89
aryangupta1 <aryangupta1@outlook.com>
2020-08-30 16:25:28 +1000

    Completed all models and migrations

--- eRestaurant/api/admin.py
+++ eRestaurant/api/admin.py
@@ -1,3 +1,14 @@
+"""
+This file essentially makes the following models visible on the django admin panel. Which allows
+us to create the respected objects, etc create a user, staff etc...
+"""
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+#Not going to register all models yet, can do later
+admin.site.register(User)
+admin.site.register(Order)
+admin.site.register(Restaurant)
+admin.site.register(Staff)
+admin.site.register(Customer)
--- eRestaurant/api/models.py
+++ eRestaurant/api/models.py
@@ -10,6 +10,9 @@ class User(models.Model):
     username = models.CharField(max_length=200, null=True)
     password = models.CharField(max_length=200, null=True) #Research into how to create Django password field
 
+    def __str__(self):
+        return self.first_name
+
 class Order(models.Model):
     order_ID = models.IntegerField(primary_key=True)
     date = models.DateTimeField(auto_now_add=True) #Auto adds date created to new orders --Aryan
@@ -17,6 +20,32 @@ class Order(models.Model):
     # Foreign key convention: name = models.ForeignKey(parentModel/ForeignKey, null=True, optional(on_delete=models.SET_NULL))
     user_ID = models.ForeignKey(User, null=True, on_delete=models.SET_NULL) #Whenever a user is deleted, the order remains with a null value , maybe we can remove this?!?
     #meal = models.ForeignKey(...) TBD
+
+class Menu(models.Model):
+    menu_ID = models.IntegerField(primary_key=True)
+
+class Item(models.Model):
+    item_ID = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=200, null=True)
+    #picture = there is a way to use an image field here, need to import pillow
+    #lunch_dinner = ?
+
+class Recipe(models.Model):
+    ingredient_ID = models.IntegerField(primary_key=True)
+    item_ID = models.ForeignKey(Item, on_delete=models.SET_NULL, null=True)
+
+class Menu_Item(models.Model):
+    menu_ItemID = models.IntegerField(primary_key=True)
+    item_1 = models.CharField(max_length = 200, null=True)
+    #item_2...
+    #item_3...
+
+class Restaurant(models.Model):
+    restaurant_ID = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100, null=True)
+    address = models.CharField(max_length=200, null = True)
+    menu_ID = models.ForeignKey(Menu, on_delete=models.SET_NULL, null=True)
+    open_hours = models.CharField(max_length=100, null=True)
 
 class Meal(models.Model):
     meal_ID = models.IntegerField(primary_key=True)
@@ -34,6 +63,16 @@ class Staff(models.Model):
     phone_number = models.IntegerField(null=True)
     email = models.EmailField(max_length = 200, null=True)
     tax_file_number = models.IntegerField()
+    #restaurant_ID
+    #date_hired
+
+class Head_Chef(models.Model):
+    user_ID = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    #access = 
+
+class Manager(models.Model):
+    user_ID = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    #access = 
 
 class Customer(models.Model):
     user_ID = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
@@ -43,4 +82,16 @@ class Customer(models.Model):
 
 class Admin(models.Model):
     user_ID = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    #access = ....
+    #access = 
+
+class Rewards(models.Model):
+    rewards_ID = models.IntegerField(primary_key=True)
+    points_percent = models.IntegerField()
+
+class Customer_Rewards(models.Model):
+    customer_rewardsID = models.IntegerField(primary_key=True)
+    user_ID = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    rewards_ID = models.ForeignKey(Rewards, on_delete=models.SET_NULL, null=True)
+
+
+    
