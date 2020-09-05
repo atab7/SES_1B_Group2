fc30264b50d5c2dc41366e5c1bc2385047abcb72
calvinlay <13254471@student.uts.edu.au>
2020-09-05 16:03:10 +1000

    added booking layout & button functionality

--- eRestaurant/frontend/src/components/HomePage.js
+++ eRestaurant/frontend/src/components/HomePage.js
@@ -16,10 +16,15 @@ import PropTypes from 'prop-types';
 import Paper from '@material-ui/core/Paper';
 import { withStyles} from '@material-ui/core/styles';
 import { grey } from '@material-ui/core/colors';
-
-
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
 import Header from './images/header7.jpg';
 import Background from './images/defaultBackground.jpg';
+import Slider from '@material-ui/core/Slider';
 
 const useStyles = makeStyles((theme) => ({
     formControl: {
@@ -50,7 +55,8 @@ var backgroundImg = {
   backgroundImage: `url(${Background})`
 };
   
-  
+
+
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
 
@@ -84,6 +90,11 @@ function a11yProps(index) {
   };
 }
 
+/*value of booking slider shows above*/
+function valuetext(value) {
+  return `${value}`;
+}
+
 const RegButton = withStyles((theme) => ({
     root: {
       color: theme.palette.getContrastText(grey[900]),
@@ -96,20 +107,72 @@ const RegButton = withStyles((theme) => ({
     },
   }))(Button);
 
+/* These are just default branches link to database later  */
+const branches = [
+  {
+    value: 'branch1',
+    label: 'location1',
+  },
+  {
+    value: 'branch2',
+    label: 'location2',
+  },
+  {
+    value: 'branch3',
+    label: 'location3',
+  },
 
+];
+
+const numOfPeople = [
+  {
+    value: '0',
+    label: '0',
+  },
+  {
+    value: '5',
+    label: '5',
+  },
+  {
+    value: '10',
+    label: '10',
+  },
+
+];
    
 
 const HomePage = () => {
-    const classes = useStyles();
-    const [branch, setBranch] = React.useState('');
-    const handleChange = (event) => {
-        setBranch(event.target.value);
-      };
-  const [value, setValue] = React.useState(0);
+  const classes = useStyles();
 
+  /* Branch select on homepage */
+  const [branch, setBranch] = React.useState('');
+  const handleChange = (event) => {
+      setBranch(event.target.value);
+    };
+  /* Tab Change on homepage */  
+  const [value, setValue] = React.useState(0);
   const handleChangetab = (event, newValue) => {
     setValue(newValue);
   };
+  /* Booking */
+  const [open, setOpenBooking] = React.useState(false);
+  const handleOpenBooking = () => {
+    setOpenBooking(true);
+  };
+  const handleCloseBooking = () => {
+    setOpenBooking(false);
+  };
+  /*Branch select on booking form */
+  const [bookingBranch, setBookingBranch] = React.useState('branch1');
+  const handleChangeBB = (event) => {
+    setBookingBranch(event.target.value);
+  };
+  /*Number of people select slider on branch*/
+  const [numpeople, setNumPeople] = React.useState(30);
+  const handleChangePeople = (event, newValue) => {
+    setNumPeople(newValue);
+  };
+  
 
     return(
         <div>
@@ -135,7 +198,7 @@ const HomePage = () => {
                         </Select>
                         <FormHelperText>Select Branch</FormHelperText>
                     </FormControl>
-                    </Box>
+                    </Box> 
                     <Box p={1} flexGrow={1}>
                     <Paper square>
                     <Tabs value={value} onChange={handleChangetab} aria-label="simple tabs example">
@@ -160,8 +223,72 @@ const HomePage = () => {
                     </TabPanel>
 
                     </Box>
+
                     <Box p={1}>
-                    <RegButton size="large">Book Now</RegButton>
+                    <RegButton size="large" onClick={handleOpenBooking}>Book Now</RegButton>
+                    <Dialog open={open} onClose={handleCloseBooking} aria-labelledby="form-dialog-title">
+                      <DialogTitle id="form-dialog-title" style={{textAlign: 'center'}}>Le Bistrot D'Andre<br/>I'd Like To Book</DialogTitle>
+                      <DialogContent>
+                       <form>
+                       <Grid container spacing={3}>
+                          <Grid item xs={12}>
+                          <TextField
+                            id="outlined-select-branch"
+                            select
+                            label="Select"
+                            value={branches}
+                            onChange={handleChangeBB}
+                            fullWidth 
+                            helperText="Please select your branch"
+                            variant="outlined"
+                          >
+                            {branches.map((option) => (
+                              <MenuItem key={option.value} value={option.value}>
+                                {option.label}
+                              </MenuItem>
+                            ))}
+                          </TextField>
+                          </Grid>
+                          <Grid item xs={12}>
+                            <Slider 
+                            min={0}
+                            max={10}
+                            step={1}
+                            value={numpeople} 
+                            onChange={handleChangePeople} 
+                            getAriaValueText={valuetext}
+                            aria-labelledby="continuous-slider"
+                            marks={numOfPeople}
+                            valueLabelDisplay="auto"
+                             />
+                          </Grid>
+                          <Grid item xs={4}>
+                            <Button variant="contained" color="primary">
+                              Today
+                            </Button>
+                          </Grid>
+                          <Grid item xs={4}>
+                            <Button variant="contained" color="primary">
+                              Tomorrow
+                            </Button>
+                          </Grid>
+                          <Grid item xs={4}>
+                            <Button variant="contained" color="primary">
+                              Select Date
+                            </Button>
+                          </Grid>
+                        </Grid>
+                       </form>
+                      </DialogContent>
+                      <DialogActions>
+                        <Button onClick={handleCloseBooking} color="primary">
+                          Cancel
+                        </Button>
+                        <Button onClick={handleCloseBooking} color="primary">
+                          Proceed to Menu
+                        </Button>
+                      </DialogActions>
+                    </Dialog> 
                     </Box>
 
                     
@@ -171,7 +298,5 @@ const HomePage = () => {
             </Box>
         </div>
     )
-
 }
-
 export default HomePage;
